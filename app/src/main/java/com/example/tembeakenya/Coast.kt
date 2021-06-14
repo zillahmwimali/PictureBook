8651d2d07330f07a1b44642cb98ad1b6384dae60
zillahmwimali <zillahmwimmali@gmail.com>
2021-06-14 13:15:20 +0300

    tembeaKenya

--- app/src/main/java/com/example/tembeakenya/Coast.kt
+++ app/src/main/java/com/example/tembeakenya/Coast.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 
 class Coast : AppCompatActivity() {
     lateinit var button7: Button
+    lateinit var button8:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coast)
@@ -14,6 +15,11 @@ class Coast : AppCompatActivity() {
         button7.setOnClickListener {
             var intent8= Intent(baseContext,Lerruat::class.java)
             startActivity(intent8)
+        }
+        button8=findViewById(R.id.button8)
+        button8.setOnClickListener {
+            var intent9= Intent(baseContext,MainActivity::class.java)
+            startActivity(intent9)
         }
     }
 }
